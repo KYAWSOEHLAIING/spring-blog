e739cbcb87b1791ccb137c0408a6f30730d8a540
Kyaw Soe Hlaing <kyawsoehlaing471994@gmail.com>
2019-07-23 11:52:05 +0630

    blogservice

--- src/main/java/com/example/demo/service/AuthorService.java
+++ src/main/java/com/example/demo/service/AuthorService.java
@@ -1,4 +1,12 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Author;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 public interface AuthorService {
+    Author create(Author author);
+    Author findById(Long id);
+    List<Author> findAll();
 }
--- src/main/java/com/example/demo/service/AuthorServiceImpl.java
+++ src/main/java/com/example/demo/service/AuthorServiceImpl.java
@@ -1,4 +1,29 @@
 package com.example.demo.service;
 
-public class AuthorServiceImpl {
+import com.example.demo.model.Author;
+import com.example.demo.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AuthorServiceImpl implements AuthorService {
+    @Autowired
+    AuthorRepository authorRepository;
+
+    @Override
+    public Author create(Author author) {
+        return authorRepository.save(author) ;
+    }
+
+    @Override
+    public Author findById(Long id) {
+        return authorRepository.getOne(id);
+    }
+
+    @Override
+    public List<Author> findAll() {
+        return authorRepository.findAll();
+    }
 }
