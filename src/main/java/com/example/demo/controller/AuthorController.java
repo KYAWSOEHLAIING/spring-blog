7572c729894757044f6c650fca6886d45271e95f
Kyaw Soe Hlaing <kyawsoehlaing471994@gmail.com>
2019-07-24 11:07:12 +0630

    blogservice

---
+++ src/main/java/com/example/demo/controller/AuthorController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Author;
+import com.example.demo.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthorController {
+    @Autowired
+    private AuthorService authorService;
+
+    @GetMapping("/author")
+    public String create(Model model){
+        model.addAttribute("author",new Author());
+        return "authorForm";
+    }
+
+    @PostMapping("/author")
+    public String process(Author author, BindingResult result){
+        if(result.hasErrors()){
+            return "authorForm";
+        }
+        authorService.create(author);
+
+        return "redirect:/authors";
+    }
+    @GetMapping("/authors")
+    public String showAllAuthor(Model model){
+        model.addAttribute("authors",authorService.findAll());
+        return "authors";
+    }
+
+
+}
